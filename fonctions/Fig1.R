fa6af443c0b32442e180cdb16b7b11388b329379
Yohan3333 <Yohan.Wegener@USherbrooke.ca>
2024-04-23 15:27:17 -0400

    commit fig1 2e

--- fonctions/Fig1.R
+++ fonctions/Fig1.R
@@ -7,12 +7,12 @@ donnees_nettoyees_lat <- bd %>%
   summarise(espece_unique = n_distinct(valid_scientific_name))
 
 # Tracer le graphique avec ggplot2 et ajout de la régression linéaire
-ggplot(donnees_nettoyees_lat, aes(x = lat, y = espece_unique)) +
+fig1 <- ggplot(donnees_nettoyees_lat, aes(x = lat, y = espece_unique)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) + # Ajout de la régression linéaire sans intervalle de confiance
   labs(x = "Latitude", y = "Nombre d'espèces uniques",
        title = "Nombre d'espèces uniques par latitude")
-ggsave("Fig1.png", plot = p, width = 10, height = 6, dpi = 300)
+ggsave("Fig1.png", plot = fig1, width = 10, height = 6, dpi = 300)
 }
 
              
