b6663fa2e00ae0740a62250b4238a5e15384fce9
Yohan3333 <Yohan.Wegener@USherbrooke.ca>
2024-04-23 15:54:22 -0400

    Fig4

--- fonctions/Fig4.R
+++ fonctions/Fig4.R
@@ -16,6 +16,18 @@ creation_Fig4 = function(bd){
   
   ###COURBE DU MODELE
   png("Figure4.png", width=1000, height=600)
-  plot(visreg::visreg(model), ylim = c(0, max(nlat2$Nombre_especes)))
+  plot(visreg::visreg(model,
+                      xlab="Latitude",
+                      ylab="Nombre d'espèce",
+                      ylim = c(0, max(nlat2$Nombre_especes)),
+                      main=("Nombre d'espèce en fonction de la latitude"),
+                      cex.lab=0.8,
+                      cex.axis=0.8,
+                      cex.main=1.5))
+  
+  r_squared <- summary(model)$r.squared
+  
+  # Add R-squared to the plot
+  text(x = mean(nlat2$Latitude), y = max(nlat2$Nombre_especes), labels = sprintf("R^2 = %.2f", r_squared), pos = 4, col = "blue", cex = 1.5)
   dev.off()
 }
