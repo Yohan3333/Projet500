f3f72a604e4abe8acd9cda4dee813b7294c3c5fb
Fredipou <stpf2507@Usherbrooke.ca>
2024-04-23 22:58:37 -0400

    modif fig 4

--- Script/Fig4.R
+++ Script/Fig4.R
@@ -1,29 +1,29 @@
-creation_Fig4 = function(bd){
+creation_Fig4 <- function(bd) {
+  # Calcul du nombre d'espèces uniques par latitude
   nlat2 <- as.data.frame(tapply(bd$species, bd$lat, function(x) length(unique(x))))
   colnames(nlat2) <- "Nombre_especes"
-  nlat2$Latitude <- rownames(nlat2)
-  nlat2 <- nlat2[, c("Latitude", "Nombre_especes")]
+  nlat2$Latitude <- as.numeric(rownames(nlat2))
   
-  ###EN NUMERIC
-  nlat2$Latitude <- as.numeric(nlat2$Latitude)
-  nlat2$Nombre_especes <- as.numeric(nlat2$Nombre_especes)
-  
-  
-  ###MODELE LINEAIRE
+  # Modèle linéaire
   model <- lm(Nombre_especes ~ Latitude, data = nlat2)
   summary(model)
+  
+  # Visualisation du modèle
   visreg::visreg(model)
   
-  ###COURBE DU MODELE
-  png("./rapport/Figure4.png", width=1000, height=600)
-  Fig4 = plot(visreg::visreg(model,
-                      xlab="Latitude",
-                      ylab="Nombre d'espèce",
-                      ylim = c(0, max(nlat2$Nombre_especes)),
-                      main=("Nombre d'espèce en fonction de la latitude"),
-                      cex.lab=0.8,
-                      cex.axis=0.8,
-                      cex.main=1.5))
-  print(Fig4)
+  # Courbe du modèle
+  png("./rapport/Figure4.png", width = 1000, height = 600)
+  plot(
+    visreg::visreg(
+      model,
+      xlab = "Latitude",
+      ylab = "Nombre d'espèce",
+      ylim = c(0, max(nlat2$Nombre_especes)),
+      main = "Nombre d'espèce en fonction de la latitude",
+      cex.lab = 0.8,
+      cex.axis = 0.8,
+      cex.main = 1.5
+    )
+  )
   dev.off()
 }
