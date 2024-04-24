d8cfc4afe66904cabf309e5c3b6e28604ffe6e53
Yohan3333 <Yohan.Wegener@USherbrooke.ca>
2024-04-23 21:42:12 -0400

    modif fig

--- Script/Fig1.R
+++ Script/Fig1.R
@@ -12,7 +12,8 @@ fig1 <- ggplot(donnees_nettoyees_lat, aes(x = lat, y = espece_unique)) +
   geom_smooth(method = "lm", se = FALSE) + # Ajout de la régression linéaire sans intervalle de confiance
   labs(x = "Latitude", y = "Nombre d'espèces uniques",
        title = "Nombre d'espèces uniques par latitude")
-ggsave("./rapport/Fig1.png", plot = fig1, width = 10, height = 6, dpi = 300)
+#ggsave("./rapport/Fig1.png", plot = fig1, width = 10, height = 6, dpi = 300)
+print(fig1)
 }
 
              
