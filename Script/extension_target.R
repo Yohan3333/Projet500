9845313a1278272ad87b8f57223ca925f5a7ae80
Yohan3333 <Yohan.Wegener@USherbrooke.ca>
2024-04-23 22:12:54 -0400

    modif

---
+++ Script/extension_target.R
@@ -0,0 +1,16 @@
+tar_target(
+  name = Fig1, #Création de la Figure 1
+  command = creation_Fig1(bd_tsn)
+),
+tar_target(
+  name=Fig2, #Création de la figure 2
+  command = creation_Fig2(bd_tsn)
+),
+tar_target(
+  name=Fig3, #Craétion de la figure 3
+  command = creation_Fig3(bd_tsn)
+),
+tar_target(
+  name=Fig4, #Création de la figure 4
+  command = creation_Fig4(bd_tsn)
+),
--- _targets.R
+++ _targets.R
@@ -67,22 +67,6 @@ list(
     name= valid_sql, #Validation des tables SQL
     command = validation_sql(table_sql)
   ),
-  tar_target(
-    name = Fig1, #Création de la Figure 1
-    command = creation_Fig1(bd_tsn)
-  ),
-  tar_target(
-    name=Fig2, #Création de la figure 2
-    command = creation_Fig2(bd_tsn)
-  ),
-  tar_target(
-    name=Fig3, #Craétion de la figure 3
-    command = creation_Fig3(bd_tsn)
-  ),
-  tar_target(
-    name=Fig4, #Création de la figure 4
-    command = creation_Fig4(bd_tsn)
-  ),
   tar_render(
     name = Rapport, # Création du rapport Rmarkdown
     path = "./rapport/rapport.Rmd"
